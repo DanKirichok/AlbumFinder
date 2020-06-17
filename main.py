157c0ec6a64b767c9ea36c3b0563b937b9fad6c4
Dan Kirichok <dan.kirichok@gmail.com>
2020-06-16 22:09:38 -0400

    Added pitchfork list

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-write_file = open("testfile.txt","w") 
+from bs4 import BeautifulSoup
 
-with open("rando.txt") as file:
-	for line in file.readlines():
-		if len(line.strip()) > 0:
-			write_file.write("'" + line.replace("\n", "") + "',\n");
+import requests
 
-write_file.close()
+def func1():
+	write_file = open("testfile.txt","w") 
+
+	with open("rando.txt") as file:
+		for line in file.readlines():
+			if len(line.strip()) > 0:
+				write_file.write("'" + line.replace("\n", "") + "',\n");
+
+	write_file.close()
+	
+def func2():
+	url = "https://pitchfork.com/features/lists-and-guides/the-200-best-albums-of-the-2010s/"
+
+	r  = requests.get(url)
+
+	data = r.text
+
+	soup = BeautifulSoup(data)
+
+	write_file = open("rando.txt","w") 
+
+	for link in soup.find_all('h2'):
+		write_file.write((link.text).replace(":", " -")[:(link.text).index("(")] + "\n")
+	
+	write_file.close()
+	
+func2()
+func1()
--- static/js/functions.js
+++ static/js/functions.js
@@ -2,7 +2,30 @@ function randomIntFromInterval(min, max) { // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+var numLists = 2;
+
+var prevAlbum = "";
+
 $("#rerollBtn").click(function(){
-	
-	$("#album-info").text(fantano_list[randomIntFromInterval(0, fantano_list.length)]);
+	var listChooser = randomIntFromInterval(0, numLists - 1);
+		
+	if (listChooser == 0) {
+		var curAlbum = pitchfork_list[randomIntFromInterval(0, pitchfork_list.length)];
+		
+		while (prevAlbum == curAlbum) {
+			curAlbum = pitchfork_list[randomIntFromInterval(0, pitchfork_list.length)];
+		}
+		
+		$("#album-info").text(curAlbum);
+		$("#list-info").text("Pitchfork");
+	} else if (listChooser == 1) {
+		var curAlbum = fantano_list[randomIntFromInterval(0, fantano_list.length)];
+		
+		while (prevAlbum == curAlbum) {
+			curAlbum = fantano_list[randomIntFromInterval(0, fantano_list.length)];
+		}
+		
+		$("#album-info").text(fantano_list[randomIntFromInterval(0, fantano_list.length)]);
+		$("#list-info").text("Fantano");
+	}
 });
